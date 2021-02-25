a29c4d7334098c4d7efe80984705d85487b25cdd
Gabor Hornyak <gabor.hornyak@msci.com>
2021-02-25 15:59:30 +0100

    alma

--- jest.config.js
+++ jest.config.js
@@ -6,5 +6,9 @@ module.exports = {
     '<rootDir>/libs/lib3',
     '<rootDir>/libs/lib4',
     '<rootDir>/libs/lib5',
+    '<rootDir>/libs/lib6',
+    '<rootDir>/libs/lib7',
+    '<rootDir>/libs/lib8',
+    '<rootDir>/libs/lib9',
   ],
 };
